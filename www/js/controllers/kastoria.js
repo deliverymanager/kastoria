4e199652cbee545786d00732b00e4fc6ad8027f6
deliverymanager <info@deliverymanager.gr>
2015-10-22 19:10:07 +0300

    Anestis

--- www/js/controllers/kastoria.js
+++ www/js/controllers/kastoria.js
@@ -1,6 +1,31 @@
 angular.module('kastoria')
-  .controller("KastoriaController",function($scope){
+  .controller("KastoriaController", function ($scope) {
 
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    $scope.myLocation = {
+      lat: '40.505038',
+      lng: '21.284637'
+    }
+
+
+    $scope.map = {
+      center: {
+        latitude: $scope.myLocation.lat,
+        longitude: $scope.myLocation.lng
+      },
+      zoom: 16,
+      pan: 1
+    };
+
+    $scope.marker = {
+      id: 0,
+      coords: {
+        latitude: $scope.myLocation.lat,
+        longitude: $scope.myLocation.lng
+      }
+    };
+
+    $scope.marker.options = {
+      draggable: false
+    };
 
   });
